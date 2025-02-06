17bca845aab554c8699d9dca562a1de54c34751e
SatooRu65536 <ryuto65536@gmail.com>
2025-02-07 02:47:07 +0900

    feat: not found page

---
+++ src/app/(not-found)/not-found.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from 'react';
+import NotFoundPage from '@/components/pages/NotfoundPage';
+import '@/styles/reset.scss';
+
+export default function NotFound(): ReactElement {
+  return <NotFoundPage />;
+}
