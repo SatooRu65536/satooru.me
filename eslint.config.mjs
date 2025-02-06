5e193cb1a689615906a28635566fc732bf3f6ab3
SatooRu65536 <ryuto65536@gmail.com>
2025-02-07 02:29:35 +0900

    fix: linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'neverthrow'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
       '@next/next/no-img-element': 'off',
