64eef9333d0725b3c43a8b4734bc72c82017ce38
SatooRu65536 <ryuto65536@gmail.com>
2025-04-06 22:46:44 +0900

    update: copyrightの年を更新

--- src/components/bases/Footer/index.tsx
+++ src/components/bases/Footer/index.tsx
@@ -2,9 +2,11 @@ import type { ReactElement } from 'react';
 import styles from './index.module.scss';
 
 export default function Footer(): ReactElement {
+  const year = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <p>SatooRu © 2024 Copyright.</p>
+      <p>SatooRu © {year} Copyright.</p>
     </footer>
   );
 }
